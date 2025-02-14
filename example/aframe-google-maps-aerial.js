bddc382e760f3af5716de99421ba76a105f2bf43
Kieran Farr <kieran.farr@gmail.com>
2025-02-14 08:33:13 -0800

    create child entity to offset ellipsoidalHeight

--- example/aframe-google-maps-aerial.js
+++ example/aframe-google-maps-aerial.js
@@ -1,7 +1,7 @@
-import { GeoUtils, WGS84_ELLIPSOID, TilesRenderer } from '3d-tiles-renderer';
+import { TilesRenderer } from '3d-tiles-renderer';
 import { TilesFadePlugin, TileCompressionPlugin, GLTFExtensionsPlugin, GoogleCloudAuthPlugin } from '3d-tiles-renderer/plugins';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
-import { MathUtils, Vector3 } from 'three';
+import { MathUtils } from 'three';
 
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before AFRAME was available.');
@@ -37,15 +37,12 @@ AFRAME.registerComponent('google-maps-aerial', {
       this.data.longitude * MathUtils.DEG2RAD
     );
 
-    // Add tiles group to the entity
-    this.el.object3D.add(this.tiles.group);
-
-    // Apply height offset to the parent object3D if provided
-    if (this.data.ellipsoidalHeight !== null) {
-      this.el.object3D.position.y = -this.data.ellipsoidalHeight;
-      this.el.object3D.updateMatrix();
-      this.el.object3D.updateMatrixWorld(true);
-    }
+    // Create a child entity for the height offset
+    const offsetEl = document.createElement('a-entity');
+    offsetEl.object3D.position.y = -this.data.ellipsoidalHeight;
+    offsetEl.object3D.add(this.tiles.group);
+    this.el.appendChild(offsetEl);
+    this.offsetEl = offsetEl;
 
     // Wait for camera and scene to be ready
     this.el.sceneEl.addEventListener('loaded', () => {
@@ -74,7 +71,10 @@ AFRAME.registerComponent('google-maps-aerial', {
 
   remove: function() {
     if (this.tiles) {
-      this.el.removeObject3D('tiles');
+      if (this.offsetEl) {
+        this.offsetEl.removeFromParent();
+        this.offsetEl = null;
+      }
       this.tiles.dispose();
       this.tiles = null;
     }
